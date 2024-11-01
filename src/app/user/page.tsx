bd552afc54495582fb15f3cc75060ba1ec139ad8
Jeremy Wright <jeremy@clerk.dev>
2024-11-01 16:32:53 -0600

    css for current user

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -4,8 +4,9 @@ export default async function Page(){
     const user = await currentUser()
 
     return (
-        <div className={"flex flex-col w-full items-center justify-center"}>
-            <pre className={"bg-zinc-700 text-orange-300"}>
+        <div className={"flex flex-col w-full items-center justify-center p-8"}>
+            <h1 className={"text-xl"}>Current User</h1>
+            <pre className={"bg-zinc-700 text-orange-300 w-full max-w-xl overflow-scroll max-h-[500px]"}>
                 {user && JSON.stringify(user, undefined, 2)}
             </pre>
         </div>
